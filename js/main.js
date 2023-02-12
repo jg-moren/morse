e7612e505b5aa5b5a5d605e9e3fdeebc491b471e
Yure Domingues <yure.domingues@gmail.com>
2023-02-11 22:33:17 -0300

    Melhorando a apresentação e criando botão de limpar texto

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const morse = document.getElementById("morse");
+const texto = document.getElementById("texto-codigo");
+
+let press = false;
+let start = Date.now();
+
+window.addEventListener('keypress', (e) => {
+    if(!press){
+        press = true;
+        start = Date.now();
+    }
+});
+
+window.addEventListener('keyup', (e) => { 
+    press = false;
+    time = Date.now() - start;
+    console.log(time);
+    texto.value += morseCode(time);
+});
+
+function morseCode(time){
+    if(time > 500){
+        return "-"
+    }
+    else{
+        return "."
+    }
+}
+
+const botaoLimpar = document.getElementById("botao-limpar");
+
+botaoLimpar.onclick = ()=>{
+    texto.value = '';
+}
